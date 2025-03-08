244deb25aca379717ea2969813fcd844952b3ba2
Luiz <luiz.albuquerque99@gmail.com>
2025-03-07 22:03:56 -0300

    adding login and register page

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+
 export function Register() {
   return (
-    <div>
-      Register
-    </div>
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">Crie sua conta</h1>
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">Já possui uma conta?</span>
+          <Link to="/login " className="tracking-[-0.5px] font-medium text-teal-900">Fazer login</Link>
+        </p>
+      </header>
+      <form action="" className="mt-[60px] flex flex-col gap-4">
+        <Input type="string" placeholder="Nome" name="nome" />
+        <Input type="email" placeholder="E-mail" name="email" />
+        <Input type="password" placeholder="Senha" name="password" />
+        <Button type="submit" className="mt-2">Criar conta</Button>
+      </form>
+    </>
   )
 }
