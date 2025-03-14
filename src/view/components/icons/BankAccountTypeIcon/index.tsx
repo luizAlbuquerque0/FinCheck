beb095b4eec8a740bef21334b0e0ba9e73364ee1
Luiz <luiz.albuquerque99@gmail.com>
2025-03-14 18:08:03 -0300

    adding iconMap

--- src/view/components/icons/BankAccountTypeIcon/index.tsx
+++ src/view/components/icons/BankAccountTypeIcon/index.tsx
@@ -1,9 +1,8 @@
-import { iconsMap } from "./iconsMap";
+import { iconsMap } from "./IconsMap";
 
 interface BankAccountTypeIconProps {
   type: keyof typeof iconsMap;
 }
-
 export function BankAccountTypeIcon({ type }: BankAccountTypeIconProps) {
   const Icon = iconsMap[type];
 
