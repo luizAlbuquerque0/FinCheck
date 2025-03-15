8299d583da68f1c1870f49df95324ee3cc87a727
Luiz <luiz.albuquerque99@gmail.com>
2025-03-15 17:01:58 -0300

    adding transaction descrptions

--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -6,10 +6,12 @@ import { MONTHS } from "../../../../../app/config/constants";
 import { cn } from "../../../../../app/utils/cn";
 import { SliderOption } from "./SliderOption";
 import { SliderNavigation } from "./SliderNavigation";
+import { formatCurrency } from "../../../../../app/utils/formatCurrency";
+import { CategoryIcon } from "../../../../components/icons/categories/CategoryIcon";
 
 export function Transactions() {
   return (
-    <div className="bg-gray-100 rounded-2xl w-full h-full p-10">
+    <div className="bg-gray-100 rounded-2xl w-full h-full p-10 flex flex-col">
       <header className="">
         <div className="flex items-center justify-between">
           <button className="flex items-center gap-2">
@@ -42,7 +44,39 @@ export function Transactions() {
         </div>
       </header>
 
-      <div className="mt-4">conteudo</div>
+      <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
+        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+          <div className="flex-1 flex items-center gap-3">
+            <CategoryIcon type="expense" />
+            <div className="">
+              <strong className="font-bold tracking-[-0.5px] block">
+                Almoco
+              </strong>
+              <strong className="text-sm text-gray-600">04/12/20202</strong>
+            </div>
+          </div>
+
+          <span className="text-red-800 font-medium tracking-[-0.5px]">
+            {formatCurrency(2000)}
+          </span>
+        </div>
+
+        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+          <div className="flex-1 flex items-center gap-3">
+            <CategoryIcon type="income" />
+            <div className="">
+              <strong className="font-bold tracking-[-0.5px] block">
+                Almoco
+              </strong>
+              <strong className="text-sm text-gray-600">04/12/20202</strong>
+            </div>
+          </div>
+
+          <span className="text-green-800 font-medium tracking-[-0.5px]">
+            {formatCurrency(2000)}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
